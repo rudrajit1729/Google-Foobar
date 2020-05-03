dde1f41789c4d7127b2ea23800f43eede5f6e97b
rudrajit <53787602+rudrajit1729@users.noreply.github.com>
2020-05-03 21:40:16 +0530

    Add files via upload

---
+++ Level 2/Please-pass-the-coded-messages.py
@@ -0,0 +1,10 @@
+# Please Pass the coded messages
+
+from itertools import combinations
+ 
+def solution(l):
+	l.sort(reverse = True)
+	for i in reversed(range(1, len(l) + 1)):
+		for tup in combinations(l, i):
+			if sum(tup) % 3 == 0: return int(''.join(map(str, tup)))
+	return 0
